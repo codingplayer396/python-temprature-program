8e462c36bfdec496eef2117753113e5f463c0d7a
codingplayer396 <skumar9915@gmail.com>
2025-07-26 14:33:45 +0530

    Add files via upload

---
+++ python temprature program/temprature.py
@@ -0,0 +1,8 @@
+temp = float(input("what's the temprature outside "))
+if temp <= 10:
+    print("its cold outdiside")
+elif temp <= 25:
+    print("its a good weather")
+elif temp > 25:
+    print("its a hot day")
+    
